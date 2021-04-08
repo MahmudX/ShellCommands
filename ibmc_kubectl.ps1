4b9f7cc20f4dc5c5e6b76a38155613588e38f956
Mahmudul Hasan <mahmudnotes@outlook.com>
2021-04-08 23:51:22 +0600

    updated kubectl

--- ibmc_kubectl.ps1
+++ ibmc_kubectl.ps1
@@ -1,5 +1,6 @@
 Clear-Host
 Set-Alias -Name kc -Value kubectl
+$region='jp2'
 
 # Get's kubernetis version
 kc version
@@ -11,7 +12,7 @@ kc config get-clusters
 kc config get-contexts
 
 # Run a image as a container in Kubernetes.
-kc run [imagename] --image [location].icr.io/[namespace]/[image name]:[version] --overrides='{"spec":{"template":{"spec":{"imagePullSecrets":[{"name":"icr"}]}}}}'
+kc run [imagename] --image $region.icr.io/[namespace]/[image name]:[version] --overrides='{"spec":{"template":{"spec":{"imagePullSecrets":[{"name":"icr"}]}}}}'
 # The --overrides option here enables us to specify the needed credentials to pull this image from IBM Cloud Container Registry.
 
 # Lists all the pods in the ns
@@ -26,3 +27,21 @@ kc describe pod [podname]
 # Delete a pod
 kc delete pod [podname]
 
+# Create pod based on configuration
+kc create -f [configuration].yaml
+
+# Set configuration as the desired state in Kubernetes
+kc apply -f [configuration].yaml
+
+# Get the list of created deployments
+kc get deployments
+
+# Exposes the deployment to the open internet
+kc expose deployment/[deploymentName] --type=NodePort --port=8080 --name=[deploymentName] --target-port=8080
+
+# Lists kubernetes services 
+kc get services
+
+# Delete deployment and service
+kc delete deployment/[deploymentName]
+kc delete service/[deploymentName]
