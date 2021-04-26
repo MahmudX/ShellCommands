b7ca809bd46b6e773ae852d6737f84dc91e087cb
Mahmudul Hasan <mahmudnotes@outlook.com>
2021-04-26 09:44:54 +0600

    Create Microsoft.PowerShell_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,7 @@
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-PoshPrompt -Theme paradox
+Set-Alias -Name ic -Value ibmcloud
+Set-Alias -Name kc -Value kubectl
+Set-Alias -Name dc -Value docker
+Set-Alias -Name dcomp -Value docker-compose
