c255dbc79657047ab1403890daa170b5580feb2e
Mahmudul Hasan <mahmudnotes@outlook.com>
2021-05-11 10:15:50 +0600

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -5,3 +5,4 @@ Set-Alias -Name ic -Value ibmcloud
 Set-Alias -Name kc -Value kubectl
 Set-Alias -Name dc -Value docker
 Set-Alias -Name dcomp -Value docker-compose
+Set-Alias -Name jlab -Value jupyter-lab
